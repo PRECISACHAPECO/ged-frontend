70c71b5e56a84c45c36a1940980afacc53d82978
jonatanK28 <jonatankalmeidakk28@gmail.com>
2023-10-05 11:56:47 -0300

    aa

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,8 +8,8 @@ import CardContent from '@mui/material/CardContent'
 import { ParametersContext } from 'src/context/ParametersContext'
 import { AuthContext } from 'src/context/AuthContext'
 import { useContext, useEffect } from 'react'
-import Factory from 'src/components/Graphics/Factory'
-import Company from 'src/components/Graphics/Company'
+import Factory from 'src/components/Graphics/home/factory'
+import Company from 'src/components/Graphics/home/company'
 
 const Home = () => {
     const { setTitle } = useContext(ParametersContext)
