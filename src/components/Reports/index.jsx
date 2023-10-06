9ac3da783f7c8f30b052e49a806a2851b3d62cd6
jonatanK28 <jonatankalmeidakk28@gmail.com>
2023-10-06 14:28:35 -0300

    aa

--- src/components/Reports/index.jsx
+++ src/components/Reports/index.jsx
@@ -1,26 +1,13 @@
-import { PDFDownloadLink, Document, Page, usePDF, BlobProvider } from '@react-pdf/renderer'
+import { PDFDownloadLink } from '@react-pdf/renderer'
 
-const MyDoc = () => (
-    <Document>
-        <Page> My document data</Page>
-    </Document>
-)
-
-const GenerateReport = () => (
+const GenerateReport = ({ title, component }) => (
     <div>
-        <PDFDownloadLink document={<MyDoc />} fileName='somename.pdf'>
-            {({ blob, url, loading, error }) => {
-                if (loading) {
-                    return 'Loading document...'
-                }
-
-                // Para abrir o PDF em uma nova guia, adicione o atributo `target="_blank"` ao link.
-                return (
-                    <a href={url} target='_blank'>
-                        Download now!
-                    </a>
-                )
-            }}
+        <PDFDownloadLink document={component} fileName='somenaaaaaaame.pdf'>
+            {({ blob, url, loading, error }) => (
+                <a href={url} target='_blank' rel='noopener noreferrer'>
+                    {loading ? 'Loading document...' : 'Download now!'}
+                </a>
+            )}
         </PDFDownloadLink>
     </div>
 )
