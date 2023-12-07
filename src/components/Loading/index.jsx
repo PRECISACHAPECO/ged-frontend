1f33c077ba143ae0db7f6b7c8442ff47683c1967
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-12-07 11:58:20 -0300

    aa

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,13 +2,13 @@ import CircularProgress from '@mui/material/CircularProgress'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
-const Loading = ({ show, title }) => {
+const Loading = ({ show, title, customTextProps, ...props }) => {
     return (
         show && (
             <div className='absolute inset-0 flex items-center justify-center rounded-lg z-50'>
                 <div className='flex flex-col justify-center items-center gap-2 '>
-                    <CircularProgress color='primary' />
-                    <p className='text-sm opacity-80'>{title ?? 'Carregando...'}</p>
+                    <CircularProgress color='primary' {...props} />
+                    <p className={`opacity-80 text-sm ${customTextProps}`}>{title ?? 'Carregando...'}</p>
                 </div>
             </div>
         )
